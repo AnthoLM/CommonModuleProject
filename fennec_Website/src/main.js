c032f6866a4152d2b13a36393776191e61a81484
Briefgarde <nemo.vollert@he-arc.ch>
2023-04-10 16:06:45 +0200

    removed the initial CSS stuff so it's controlled only by bootswatch now.

--- fennec_Website/src/main.js
+++ fennec_Website/src/main.js
@@ -2,11 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import "bootstrap";
-//import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import "bootswatch/dist/darkly/bootstrap.min.css";
+import "bootswatch/dist/morph/bootstrap.min.css";
 
-import './assets/main.css'
+
 
 const app = createApp(App)
 
