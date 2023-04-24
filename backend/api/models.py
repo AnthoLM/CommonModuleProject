a66548d9a8e586f0ce586e63838b558546f847d0
Briefgarde <nemo.vollert@he-arc.ch>
2023-04-24 21:58:57 +0200

    added a few comments on models for the futur

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Event(models.Model):
+    #All the blanking is to make the fields optional in the admin panel.
+    #From the front end, the fields will be required. The users must not be able to click on the send button without filling the fields.
     title = models.CharField(max_length=100)
     description = models.TextField(default="", blank=True)
     dateDebut = models.DateTimeField(null=True)
@@ -9,8 +11,8 @@ class Event(models.Model):
     allDay = models.BooleanField(default=False)
     streetName = models.CharField(max_length=100, default="", blank=True)
     streetNumber = models.CharField(max_length=10, default="", blank=True)
-    AgeCategory = models.ForeignKey('AgeCategory', on_delete=models.CASCADE, null=True)
-    EventCategory = models.ForeignKey('EventCategory', on_delete=models.CASCADE, null=True)
+    AgeCategory = models.ForeignKey('AgeCategory', on_delete=models.CASCADE, null=True) #Will probably need a related name
+    EventCategory = models.ForeignKey('EventCategory', on_delete=models.CASCADE, null=True) #Will probably need a related name
     owner = models.ForeignKey('auth.User', related_name='events', on_delete=models.CASCADE, null=True) # One to many relationship (One user can have many events)
     #This need works and can not be used as is. 
     
