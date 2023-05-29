1d12e4276f73734d0fa652996a34c8b35e85643c
Briefgarde <nemo.vollert@he-arc.ch>
2023-05-29 12:33:34 +0200

    fixed problem on pageDetail + commented out a problem lib

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import "bootstrap/dist/css/bootstrap.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
 
 import "@fortawesome/fontawesome-free/css/all.min.css"
-import 'bootstrap/scss/bootstrap.scss';
+//import 'bootstrap/scss/bootstrap.scss';
 
 const app = createApp(App)
 app.use(router)
