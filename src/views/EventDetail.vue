ae483859ec44c856488151002ac3c1f2194d6d2b
Briefgarde <nemo.vollert@he-arc.ch>
2023-05-30 20:35:22 +0200

    added registration methods to be ready for integration

--- src/views/EventDetail.vue
+++ src/views/EventDetail.vue
@@ -11,7 +11,7 @@
                 <h1>{{ event.name }}</h1>
                 <p class="border">{{ event.description }}</p>
                 <p class="border">This event will start on the {{ formatDate(event.startDate) }} and will end the {{ formatDate(event.endDate) }}</p>
-                <button type="button" class="btn btn-primary" v-if="user">Register</button>
+                <button type="button" class="btn btn-primary" v-if="user" @click="register" :disabled="isRegistrationPossible">{{textButtonRegister}}</button>
                 <hr>
             </div>
         </div>
@@ -22,6 +22,7 @@
 import authService from "@/services/authService"
 import decoration from "../models/decoration"
 import eventService from "@/services/eventService"
+//import registerToEventService from "@/services/registerToEventService"
 
 export default {
   data() {
@@ -30,7 +31,7 @@ export default {
       idEvent: this.$route.params.id,
       commentContent: "",
       event:"",
-      comments: [],
+      registration: [],
     }
   },
   computed:{
@@ -40,9 +41,24 @@ export default {
     isEventLoading() {
       return this.event === ""
     },
+    isRegistrationPossible() {
+      if (this.registration !== null){
+        return this.event.maxParticipants > this.registration.length
+      } else return false
+    },
+    textButtonRegister(){
+      if (!this.isRegistrationPossible){
+        return "Register"
+      } else return "Event at capacity"
+    }
   },
   async mounted() {
     this.event = await eventService.getEvent(this.idEvent)
+  },
+  watch: {
+    event() {
+      //this.registration = registerToEventService.getRegisterId(this.idEvent)
+    },
   },
   methods: {
     formatDate(dateString) {
@@ -55,6 +71,12 @@ export default {
         };
         const date = new Date(dateString);
         return date.toLocaleString("en-CH", options);
+    },
+    register() {
+      // registerToEventService.addRegister({
+      //   user: decoration.path + "register_to_Event/" + this.user.pk + "/",
+      //   event: decoration.path + "register_to_Event/" + this.event.id + "/",
+      // })
     },
   },
 }
