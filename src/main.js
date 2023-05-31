8fcea61e047a856d067460357efe8058716f9c3f
Briefgarde <nemo.vollert@he-arc.ch>
2023-05-31 14:59:46 +0200

    some change to accomodate for bootswatch

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from "./router"
 import "bootstrap/dist/css/bootstrap.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
 import "@fortawesome/fontawesome-free/css/all.min.css"
+import 'bootswatch/dist/United/bootstrap.min.css';
 //import 'bootstrap/scss/bootstrap.scss';
 
 const app = createApp(App)
