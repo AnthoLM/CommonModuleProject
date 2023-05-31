5fd3fdc47325dbb204a479ed9e839cb831c14334
Briefgarde <nemo.vollert@he-arc.ch>
2023-05-31 16:17:07 +0200

    added comments on events

--- src/views/EventDetail.vue
+++ src/views/EventDetail.vue
@@ -16,6 +16,43 @@
                 <hr>
             </div>
         </div>
+        <div class="row">
+          <div class="col-8 offset-2">
+            <h2>Commentaires</h2>
+            <div v-if="!user">
+              <p>You need to be logged in to comment !</p>
+              <RouterLink class="btn btn-primary" to="/login">Login</RouterLink>
+            </div>
+            <div v-else>
+              <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Ton commentaire" maxlength="500" v-model="commentContent">
+                <button 
+                class="btn btn-outline-secondary" 
+                type="button" id="button-addon2" 
+                v-on:click="sendMessage()"
+                  >Envoyer !
+                </button>
+              </div>
+            </div>
+            <hr>
+            <div v-if="filteredComments.length === 0">
+          <div>Looks like there are no comments for this place yet !</div>
+        </div>
+        <div v-else class="row">
+          <div class="card text-center mt-3 " v-for="comment in filteredComments" :key="comment.id">
+            <div class="card-header">
+              Comment by {{comment.user.username}}
+            </div>
+            <div class="card-body">
+              <p>{{ comment.content }}</p>
+            </div>
+            <div class="card-footer text-muted">
+              {{ formatDate(comment.date) }}
+            </div>
+          </div>
+        </div>
+          </div>
+        </div>
     </div>
 </template>
 
@@ -24,6 +61,7 @@ import authService from "@/services/authService"
 import decoration from "../models/decoration"
 import eventService from "@/services/eventService"
 import registerToEventService from "@/services/registerToEventService"
+import commentService from "@/services/commentService"
 
 export default {
   data() {
@@ -34,6 +72,7 @@ export default {
       event:"",
       registration: null,
       instantFeebackRegistration: false,
+      comments: [],
     }
   },
   computed:{
@@ -63,6 +102,11 @@ export default {
       } else {
         return "Event at capacity"
       }
+    },
+    filteredComments(){
+      if (this.comments !== null){
+        return this.comments.filter((comment) => comment.event === decoration.path + "events/" + this.event.id + "/")
+      } else return []
     }
   },
   async mounted() {
@@ -71,7 +115,13 @@ export default {
   watch: {
     async event() {
       this.registration = await registerToEventService.getRegisterId(this.event.id)
+      this.comments = await commentService.getEventComments(this.event.id)
     },
+    async comments() {
+      if (this.comments !== null) {
+        this.comments = await commentService.getEventComments()
+      }
+    }
   },
   methods: {
     formatDate(dateString) {
@@ -92,6 +142,16 @@ export default {
       })
       this.instantFeebackRegistration = true
     },
+    sendMessage(){
+      let payload = {
+        user : decoration.path + "users/" + this.user.pk + "/",
+        event: decoration.path + "events/" + this.event.id+"/",
+        content: this.commentContent,
+      }
+      commentService.addEventComment(payload)
+      this.comments.push(payload)
+      this.commentContent = ""
+    }
   },
 }
 
