b7c916e68f09aa23989f6338fe419c587900f2b8
Briefgarde <nemo.vollert@he-arc.ch>
2023-06-01 13:05:14 +0200

    added a built version of the baseURL path, for later...

--- src/models/decoration.js
+++ src/models/decoration.js
@@ -2,7 +2,8 @@
 
 const webSiteSettings = {
   websitename: "Fennec Meetup",
-  path: "http://localhost:8000/api/"
+  path: "http://localhost:8000/api/",
+  //pathBuilt : "https://fennec.rxq.ch/api/",
 }
 export default webSiteSettings
 //The goal of this is to keep all our frontend variable like name and others things neatly in the same place, so they are easy to change later on.
